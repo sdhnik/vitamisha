cfa4e75ed454a137543fbaaced34ed736ba8ec15
aleksey.severnyuk <aleksey.severnyuk@uvoteam.com>
2020-03-01 22:00:44 +0200

    fix

--- wp-content/themes/vitamisha/js/main.js
+++ wp-content/themes/vitamisha/js/main.js
@@ -1,8 +1,38 @@
 (function () {
 
-	new Glide('.glide', {
+	var glide = new Glide('.glide', {
 		startAt: 0,
 		perView: 3
-	}).mount();
+	});
+
+	if(window.innerWidth>680 && window.innerWidth<992) {
+		glide.update({ perView: 2 });
+	} else if(window.innerWidth<680) {
+		glide.update({ perView: 1 });
+	} else {
+		glide.update({ perView: 3 });
+	}
+
+	glide.on('resize', function() {
+		if(window.innerWidth>680 && window.innerWidth<992) {
+			glide.update({ perView: 2 });
+		} else if(window.innerWidth<680) {
+			glide.update({ perView: 1 });
+		} else {
+			glide.update({ perView: 3 });
+		}
+
+		var currentBlock = document.querySelector('.product_list_widget'),
+			currentWidth = currentBlock.querySelector('.glide__slide--active'),
+			currentSvg = currentBlock.querySelectorAll('.button--dashes');
+
+		for (var i = 0; i < currentSvg.length; i++) {
+			var currentRect = currentSvg[i].querySelector('rect');
+			currentRect.setAttribute('width', currentWidth.offsetWidth-10);
+			currentRect.setAttribute('height', currentWidth.offsetHeight-10);
+		};
+	});
+
+	glide.mount();
 
 })();
